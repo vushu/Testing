1828cd67ec7f582b953c0d3545d451f3a2718ad8
vushu <danvu.hustle@gmail.com>
2013-10-22 10:29:40 +0200

    test

--- Fsharpvu/Program.fs
+++ Fsharpvu/Program.fs
@@ -2,10 +2,14 @@
 // See the 'F# Tutorial' project for more help.
 
 let gange b = b * b
-
+let filt x = (x%2 = 1)
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     printfn "%d" (gange 4)
+    let ma = System.Console.ReadLine()
+    let res = [1..10]
+    let filteret = List.filter filt res 
+    printfn  "%d" filteret.Length
     let ma = System.Console.ReadLine()
     0 // return an integer exit code
