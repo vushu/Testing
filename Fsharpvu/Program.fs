d7e98a6aee509694ea0ad55c510fd725b2820206
vushu <danvu.hustle@gmail.com>
2013-10-22 09:56:14 +0200

    tester git

--- Fsharpvu/Program.fs
+++ Fsharpvu/Program.fs
@@ -1,7 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+let gange b = b * b
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    printfn "%d" (gange 4)
     0 // return an integer exit code
